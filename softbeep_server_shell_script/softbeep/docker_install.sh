59673ddc8e8dfcc94aa27a28de39857ca2446891
jamangstangs <jmsong3807@gm.gist.ac.kr>
2022-08-06 16:13:16 +0900

    서버 자동화 스크립트 업로드

---
+++ softbeep_server_shell_script/softbeep/docker_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "=====Docker Install====="
+
+# Setup Repository
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupglsb-release
+
+# Add docker gpg key
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install docker engine
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
